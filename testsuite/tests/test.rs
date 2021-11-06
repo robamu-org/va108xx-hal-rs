8b3d69dae71f5d2996508e9017816266d43e69bd
Robin Mueller <robin.mueller.m@gmail.com>
2021-11-06 20:10:18 +0100

    problems with probe-run

--- testsuite/tests/test.rs
+++ testsuite/tests/test.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![no_main]
 
+use defmt_rtt as _;    // transport layer for defmt logs
+use panic_probe as _;  // panicking behavior
+use va108xx_hal as _;
+
 #[defmt_test::tests]
 mod tests {
     use defmt::assert;
